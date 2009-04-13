d7cbc6a6af0b8a8822761ca7fc793eb84ff90dd2
megbeckIM <megbeckIM@9ff7648e-257f-11de-9b37-5dcb32d212a7>
2009-04-13 12:00:41 +0000

    Added some javadoc.

--- src/net/nycjava/skylight/dependencyinjection/Dependency.java
+++ src/net/nycjava/skylight/dependencyinjection/Dependency.java
@@ -5,8 +5,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Use this annotation on any field (regardless of access, and regardless of being static or not).
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface Dependency {
-
 }
