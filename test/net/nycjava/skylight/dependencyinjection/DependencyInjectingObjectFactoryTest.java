9744805dff07550e6eb7f5b70650630983ec98ad
megbeckim <megbeckIM@gmail.com>
2009-04-17 16:08:59 +0000

    Improved DI framework and DI framework tests, and included examples of DI in two Activity classes.  Demonstrated example of navigating from one Activity to another via Intents.

--- test/net/nycjava/skylight/dependencyinjection/DependencyInjectingObjectFactoryTest.java
+++ test/net/nycjava/skylight/dependencyinjection/DependencyInjectingObjectFactoryTest.java
@@ -4,65 +4,181 @@ import junit.framework.TestCase;
 
 public class DependencyInjectingObjectFactoryTest extends TestCase {
 	static interface Y {
-		int square(int aValue);
+		void setValue(int aValue);
+
+		int getValue();
+
+		X getX1();
+
+		X getX2();
 	}
 
-	static class X implements Y {
-		static X lastXCreated;
-
-		public X() {
-			lastXCreated = this;
-		}
+	static class YImpl implements Y {
+		private int value;
 
 		@Dependency
-		private Y foo;
+		private X x1;
 
-		private Y bar;
+		private X x2;
+
+		public void setValue(int aValue) {
+			value = aValue;
+		}
+
+		public int getValue() {
+			return value;
+		}
 
 		@Override
-		public int square(int aValue) {
-			return aValue * aValue;
+		public X getX1() {
+			return x1;
+		}
+
+		@Override
+		public X getX2() {
+			return x2;
+		}
+	}
+
+	static interface X {
+
+		Y getY1();
+
+		Y getY2();
+	}
+
+	static class XImpl implements X {
+		@Dependency
+		private Y y1;
+
+		private Y y2;
+
+		@Override
+		public Y getY1() {
+			return y1;
+		}
+
+		@Override
+		public Y getY2() {
+			return y2;
+		}
+	}
+
+	static interface Z {
+		Y getY1();
+
+		Y getY2();
+	}
+
+	static class ZImpl implements Z {
+		@Dependency
+		private YImpl y1;
+
+		private YImpl y2;
+
+		@Override
+		public Y getY1() {
+			return y1;
+		}
+
+		@Override
+		public Y getY2() {
+			return y2;
 		}
 	}
 
 	public void testRegisterImplementationClass() {
+		// register implementations
 		DependencyInjectingObjectFactory dependencyInjectingObjectFactory = new DependencyInjectingObjectFactory();
-		dependencyInjectingObjectFactory.registerImplementationClass(Y.class, X.class);
-		Y priorLastXCreated = X.lastXCreated;
-		Y y = dependencyInjectingObjectFactory.getObject(Y.class);
-		assertSame(priorLastXCreated, X.lastXCreated);
-		assertEquals(100, y.square(10));
-		assertNotSame(priorLastXCreated, X.lastXCreated);
-		assertNotNull(X.lastXCreated.foo);
-		assertNull(X.lastXCreated.bar);
+		dependencyInjectingObjectFactory.registerImplementationClass(Y.class, YImpl.class);
+		dependencyInjectingObjectFactory.registerImplementationClass(X.class, XImpl.class);
+
+		// obtain two instances
+		Y y1 = dependencyInjectingObjectFactory.getObject(Y.class);
+		y1.setValue(100);
+		Y y2 = dependencyInjectingObjectFactory.getObject(Y.class);
+		y2.setValue(200);
+
+		// make sure they are different objects
+		assertEquals(100, y1.getValue());
+		assertEquals(200, y2.getValue());
+
+		// make sure their dependencies were set correctly
+		assertNotNull(y1.getX1());
+		assertNull(y1.getX2());
+		assertNotNull(y2.getX1());
+		assertNull(y2.getX2());
 	}
 
 	public void testRegisterSingletonImplementationClass() {
+		// register implementations
 		DependencyInjectingObjectFactory dependencyInjectingObjectFactory = new DependencyInjectingObjectFactory();
-		dependencyInjectingObjectFactory.registerSingletonImplementationClass(Y.class, X.class);
-		Y priorLastXCreated = X.lastXCreated;
-		Y y = dependencyInjectingObjectFactory.getObject(Y.class);
-		assertSame(priorLastXCreated, X.lastXCreated);
-		assertEquals(100, y.square(10));
-		assertNotSame(priorLastXCreated, X.lastXCreated);
-		assertNotNull(X.lastXCreated.foo);
-		assertNull(X.lastXCreated.bar);
-		Y moreRecentPriorLastXCreated = X.lastXCreated;
-		dependencyInjectingObjectFactory.getObject(Y.class);
-		assertSame(moreRecentPriorLastXCreated, X.lastXCreated);
+		dependencyInjectingObjectFactory.registerSingletonImplementationClass(Y.class, YImpl.class);
+		dependencyInjectingObjectFactory.registerSingletonImplementationClass(X.class, XImpl.class);
+
+		// obtain two instances
+		Y y1 = dependencyInjectingObjectFactory.getObject(Y.class);
+		y1.setValue(100);
+		Y y2 = dependencyInjectingObjectFactory.getObject(Y.class);
+		y2.setValue(200);
+
+		// make sure they are the same object
+		assertEquals(200, y1.getValue());
+		assertEquals(200, y2.getValue());
+
+		// make sure its dependencies were set correctly
+		assertNotNull(y1.getX1());
+		assertNull(y1.getX2());
 	}
 
-	public void testRegisterImplementationObject() {
+	public void testRegisterImplementationObjectForInterface() {
+		// register implementations
 		DependencyInjectingObjectFactory dependencyInjectingObjectFactory = new DependencyInjectingObjectFactory();
-		X x = new X();
-		dependencyInjectingObjectFactory.registerImplementationObject(Y.class, x);
-		assertNull(x.foo);
-		assertNull(x.bar);
-		Y y = dependencyInjectingObjectFactory.getObject(Y.class);
-		assertNull(x.foo);
-		assertNull(x.bar);
-		assertEquals(100, y.square(10));
-		assertNotNull(x.foo);
-		assertNull(x.bar);
+		dependencyInjectingObjectFactory.registerImplementationObject(Y.class, new YImpl());
+		dependencyInjectingObjectFactory.registerImplementationObject(X.class, new XImpl());
+
+		// obtain two instances
+		Y y1 = dependencyInjectingObjectFactory.getObject(Y.class);
+		y1.setValue(100);
+		Y y2 = dependencyInjectingObjectFactory.getObject(Y.class);
+		y2.setValue(200);
+
+		// make sure they are the same object
+		assertEquals(200, y1.getValue());
+		assertEquals(200, y2.getValue());
+
+		// make sure its dependencies were set correctly
+		assertNotNull(y1.getX1());
+		assertNull(y1.getX2());
 	}
+
+	public void testRegisterImplementationObjectForClass() {
+		// register implementation
+		DependencyInjectingObjectFactory dependencyInjectingObjectFactory = new DependencyInjectingObjectFactory();
+		dependencyInjectingObjectFactory.registerImplementationObject(Z.class, new ZImpl());
+		dependencyInjectingObjectFactory.registerImplementationObject(YImpl.class, new YImpl());
+
+		// obtain an instance
+		Z z = dependencyInjectingObjectFactory.getObject(Z.class);
+
+		// make sure its dependencies were set correctly
+		assertNotNull(z.getY1());
+		assertNull(z.getY2());
+	}
+
+	public void testCyclesAreSafe() {
+		// register implementation
+		DependencyInjectingObjectFactory dependencyInjectingObjectFactory = new DependencyInjectingObjectFactory();
+		YImpl y = new YImpl();
+		dependencyInjectingObjectFactory.registerImplementationObject(Y.class, y);
+		XImpl x = new XImpl();
+		dependencyInjectingObjectFactory.registerImplementationObject(X.class, x);
+
+		// obtain an instance
+		Y y1 = dependencyInjectingObjectFactory.getObject(Y.class);
+
+		// check that cycles are ok
+		assertNotNull(y1.getX1().getY1().getX1().getY1());
+	}
+
 }
