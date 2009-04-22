f0cb964bd3da1909cd3e682c7cd5917661fe8ee7
megbeckim <megbeckIM@gmail.com>
2009-04-22 04:19:43 +0000

    screen prototype

---
+++ Skylight TDC/src/net/nycjava/skylight/tdc/SkylightTDC.java
@@ -0,0 +1,13 @@
+package net.nycjava.skylight.tdc;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SkylightTDC extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
