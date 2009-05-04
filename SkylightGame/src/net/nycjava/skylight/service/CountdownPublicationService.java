652f6677c08ad3501e989e0ab57433cea18c2c74
tcynth <tcynth@9ff7648e-257f-11de-9b37-5dcb32d212a7>
2009-05-04 19:40:08 +0000

    added implementation for CountdownPublicationServiceImpl. -CT

--- SkylightGame/src/net/nycjava/skylight/service/CountdownPublicationService.java
+++ SkylightGame/src/net/nycjava/skylight/service/CountdownPublicationService.java
@@ -3,4 +3,13 @@ package net.nycjava.skylight.service;
 
 public interface CountdownPublicationService extends Observable<CountdownObserver> {
 
+	public void startCountdown();
+	public void setDuration(int time); //in seconds	
+	public void stopCountdown();
+	public counter_status getStatus();
+	
+	public void addObserver(CountdownObserver anObserver);
+	public boolean removeObserver(CountdownObserver anObserver);
+	
+	public void joinThread() throws InterruptedException; //for testing purpose
 }
