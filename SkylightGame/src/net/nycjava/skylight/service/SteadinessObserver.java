1936ab09a9563cfb2ffde007aa3c7c666363b370
michaelriesel <michaelriesel@9ff7648e-257f-11de-9b37-5dcb32d212a7>
2009-05-06 18:55:53 +0000

    implement changes discussed in last meeting

--- SkylightGame/src/net/nycjava/skylight/service/SteadinessObserver.java
+++ SkylightGame/src/net/nycjava/skylight/service/SteadinessObserver.java
@@ -5,5 +5,8 @@ public interface SteadinessObserver {
 	 * Called intermittently to notify the observer of the steadiness of the device. Forces are measured in centimeters
 	 * per second squared and angles in degrees.
 	 */
-	void steadinessNotification(float paremetersThatICantThinkOfRightNow);
+	void steadinessNotification(float force);
+	
+	 // called to notify unsteadiness
+	void unsteadinessNotification();
 }
