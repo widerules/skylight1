d9debc880e0eece32d5b8dfe00da9c05f9488bf3
tcynth <tcynth@9ff7648e-257f-11de-9b37-5dcb32d212a7>
2009-05-08 01:55:55 +0000

    first trial version of fail activity

--- SkylightGame/src/net/nycjava/skylight/FailActivity.java
+++ SkylightGame/src/net/nycjava/skylight/FailActivity.java
@@ -1,16 +1,41 @@
 package net.nycjava.skylight;
 
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
 import net.nycjava.skylight.dependencyinjection.DependencyInjectingObjectFactory;
+import net.nycjava.skylight.view.Preview;
 
 /**
  * reporting unsteady hand; report acknowledged; reporting slow hand; report acknowledged; go to welcome
  */
 public class FailActivity extends SkylightActivity {
 
+	private View view;
+	
 	@Override
 	protected void addDependencies(DependencyInjectingObjectFactory dependencyInjectingObjectFactory) {
-		// TODO Auto-generated method stub
+		dependencyInjectingObjectFactory.registerImplementationObject(Camera.class, Camera.open());
+		dependencyInjectingObjectFactory.registerImplementationObject(View.class, getLayoutInflater().inflate(
+				R.layout.failMsg, null));
+		dependencyInjectingObjectFactory.registerImplementationObject(Preview.class, new Preview(this));
 
 	}
 
+	
+	 /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		// Hide the window title.
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+		setContentView(view);
+
+		// Create our Preview view and set it as the content of our activity.
+		// mPreview = new Preview(this);
+		// setContentView(mPreview);
+	}
 }
