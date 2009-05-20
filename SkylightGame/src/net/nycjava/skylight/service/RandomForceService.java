da2887368ed30377d780d0bc10b920d5241ce3fc
megbeckim <megbeckIM@gmail.com>
2009-05-20 02:58:35 +0000

    more ideas

---
+++ SkylightGame/src/net/nycjava/skylight/service/RandomForceService.java
@@ -0,0 +1,8 @@
+package net.nycjava.skylight.service;
+
+/**
+ * Applies random forces at random times to a BalancedObjectPublicationService.
+ */
+public interface RandomForceService {
+
+}
---
+++ SkylightGame/src/net/nycjava/skylight/service/SensorAppliedForceAdapter.java
@@ -0,0 +1,9 @@
+package net.nycjava.skylight.service;
+
+/**
+ * Depends on a BalancedObjectPublicationService and a SensorManager. Adapts SensorManager events to apply forces to a
+ * BalancedObjectPublicationService.
+ */
+public interface SensorAppliedForceAdapter {
+
+}
