e5199c96e294b4ac2c824e161331373aa51e1c65
megbeckim <megbeckIM@gmail.com>
2009-05-20 03:04:01 +0000

    added start and stop

--- SkylightGame/src/net/nycjava/skylight/service/RandomForceService.java
+++ SkylightGame/src/net/nycjava/skylight/service/RandomForceService.java
@@ -4,5 +4,7 @@ package net.nycjava.skylight.service;
  * Applies random forces at random times to a BalancedObjectPublicationService.
  */
 public interface RandomForceService {
-
+	void start();
+	
+	void stop();
 }
