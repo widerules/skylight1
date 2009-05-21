f4bb01df8ea5f764d52f2a2abd581df9c9f795e1
megbeckim <megbeckIM@gmail.com>
2009-05-21 23:46:35 +0000

    remove the new methods on the interface - access for testing can be through the class

--- SkylightGame/src/net/nycjava/skylight/service/BalancedObjectPublicationService.java
+++ SkylightGame/src/net/nycjava/skylight/service/BalancedObjectPublicationService.java
@@ -2,9 +2,4 @@ package net.nycjava.skylight.service;
 
 public interface BalancedObjectPublicationService extends Observable<BalancedObjectObserver> {
 	void applyForce(float anAngleInRadians, float aForceInNewtons);
-	void resetCurrentCondition(float anAngleInRadians, float aForceInNewtons);
-	float getCurrentAngle();	
-	float getCurrentMagnitude();	
-	public float getCurrentLeanInRadians();
-
 }
