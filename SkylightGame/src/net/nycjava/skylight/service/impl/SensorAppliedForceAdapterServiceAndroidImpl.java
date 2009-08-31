c8fe8223029e608aef17bdfb58773a4b3df5a688
megbeckim <megbeckIM@gmail.com>
2009-08-31 13:12:19 +0000

    removed unused variable/comments/code, and in particular, allocations for objects that were unused

--- SkylightGame/src/net/nycjava/skylight/service/impl/SensorAppliedForceAdapterServiceAndroidImpl.java
+++ SkylightGame/src/net/nycjava/skylight/service/impl/SensorAppliedForceAdapterServiceAndroidImpl.java
@@ -12,19 +12,13 @@ public class SensorAppliedForceAdapterServiceAndroidImpl implements SensorApplie
 	private static final int Y_AXIS = 1;
 
 	private static final int X_AXIS = 0;
-	
-	private static final int Z_AXIS = 2;
-
-	private static final float MILLISECONDS_IN_A_SECOND = 1000f;
 
 	private static final float FORCE_FACTOR = 1.0f;
-	
-	private static final int MILLISEC_WAIT_FOR_CALIBRATE = 300;
-	
+
 	private static final int CALIBRATE_MAX_COUNT = 10;
 
 	private static final String TAG = "SensorAppliedForceAdapterServiceAndroidImpl";
-	
+
 	@Dependency
 	BalancedObjectPublicationService balancedPublicationService;
 
@@ -32,40 +26,43 @@ public class SensorAppliedForceAdapterServiceAndroidImpl implements SensorApplie
 	private SensorManager mSensorManager;
 
 	private long lastTime;
-	private long calibrateDoneTime;
+
 	private double sumX;
+
 	private double sumY;
+
 	private int countXY;
+
 	private boolean calibrateDone;
-	private float baseX;
-	private float baseY;
+
 	private int calibrateCount;
+
 	private float lowX;
+
 	private float highX;
+
 	private float lowY;
+
 	private float highY;
-	
+
 	private final SensorListener mListener = new SensorListener() {
 		public void onSensorChanged(int sensor, float[] values) {
 			final long thisTime = System.currentTimeMillis();
-			final float scaledForceFactor = (float) (thisTime - lastTime) / MILLISECONDS_IN_A_SECOND;
-			float x = values[X_AXIS] ;
-			float y = values[Y_AXIS] ;			
-			float z = values[Z_AXIS] ;
-			StringBuffer valuesOut = new StringBuffer();
-			if(calibrateDone == false) {
-				if(Math.abs(x) > 5.0 || Math.abs(y) > 5.0 ) {
-					//User is holding the phone vertically (or at least > 30 deg) 
-					//so put some 'good' defaults into range and let glass drop
+			float x = values[X_AXIS];
+			float y = values[Y_AXIS];
+			if (calibrateDone == false) {
+				if (Math.abs(x) > 5.0 || Math.abs(y) > 5.0) {
+					// User is holding the phone vertically (or at least > 30 deg)
+					// so put some 'good' defaults into range and let glass drop
 					lowX = -0.005f;
 					highX = 0.005f;
 					lowY = -0.005f;
 					highY = 0.005f;
-					calibrateDone = true;					
+					calibrateDone = true;
 				} else {
 					// user is holding holding the phone facing the sky (more or less)
-					if (calibrateCount < CALIBRATE_MAX_COUNT ) {
-						//TODO: replace accl calibration with time weighted average
+					if (calibrateCount < CALIBRATE_MAX_COUNT) {
+						// TODO: replace accl calibration with time weighted average
 						// do calibration of x,y
 						setXRange(x);
 						setYRange(y);
@@ -73,40 +70,21 @@ public class SensorAppliedForceAdapterServiceAndroidImpl implements SensorApplie
 						sumY += y;
 						countXY++;
 						calibrateCount++;
-//						valuesOut.append("calibrate Entry ");
-//						valuesOut.append(x);
-//						valuesOut.append(" ");
-//						valuesOut.append(y);
-						//Log.d("SensorListener",valuesOut.toString());
 					} else {
-						//baseX = (float) (sumX / countXY);
-						//baseY = (float) (sumY / countXY);
 						calibrateDone = true;
-//						valuesOut.append("calibrate done");
-//						valuesOut.append(lowX);
-//						valuesOut.append(" ");
-//						valuesOut.append(highX);
-//						valuesOut.append(" ");
-//						valuesOut.append(lowY);
-//						valuesOut.append(" ");
-//						valuesOut.append(highY);
-
-						//Log.d("SensorListener",valuesOut.toString());
 					}
 				}
 			} else {
 				// calibration done so use it.
-				//x = x - baseX;
-				//y = y - baseY;
-				if(true) {
-					if(x < lowX) {
+				if (true) {
+					if (x < lowX) {
 						x = x - lowX;
 					} else if (x > highX) {
 						x = x - highX;
 					} else {
 						x = 0.0f;
 					}
-					if(y < lowY) {
+					if (y < lowY) {
 						y = y - lowY;
 					} else if (y > highY) {
 						y = y - highY;
@@ -116,13 +94,7 @@ public class SensorAppliedForceAdapterServiceAndroidImpl implements SensorApplie
 					x = x * FORCE_FACTOR;
 					y = y * FORCE_FACTOR;
 				}
-//				valuesOut.append(x);
-//				valuesOut.append(" ");
-//				valuesOut.append(y);
-//				valuesOut.append(" ");
-//				valuesOut.append(z);
-				//Log.d("SensorListener ",valuesOut.toString());
-				balancedPublicationService.applyForce(x, -y,(thisTime - lastTime));
+				balancedPublicationService.applyForce(x, -y, (thisTime - lastTime));
 			}
 			lastTime = thisTime;
 		}
@@ -135,7 +107,6 @@ public class SensorAppliedForceAdapterServiceAndroidImpl implements SensorApplie
 		int mask = 0;
 		mask |= SensorManager.SENSOR_ACCELEROMETER;
 		lastTime = System.currentTimeMillis();
-		calibrateDoneTime = lastTime + MILLISEC_WAIT_FOR_CALIBRATE;
 		sumX = 0;
 		sumY = 0;
 		countXY = 0;
@@ -146,30 +117,29 @@ public class SensorAppliedForceAdapterServiceAndroidImpl implements SensorApplie
 		calibrateCount = 0;
 		calibrateDone = false;
 		mSensorManager.registerListener(mListener, mask, SensorManager.SENSOR_DELAY_GAME);
-		Log.d(TAG,"start");
+		Log.d(TAG, "start");
 	}
 
 	public void stop() {
 		mSensorManager.unregisterListener(mListener);
-		Log.d(TAG,"stop");
+		Log.d(TAG, "stop");
 	}
-	
+
 	private void setXRange(float x) {
-		if((x < lowX) || (lowX == 999)) {
+		if ((x < lowX) || (lowX == 999)) {
 			lowX = x;
 		}
-		if((x > highX) || (highX == -999)) {
-			highX = x;			
+		if ((x > highX) || (highX == -999)) {
+			highX = x;
 		}
 	}
 
 	private void setYRange(float y) {
-		if((y < lowY) || (lowY == 999)) {
+		if ((y < lowY) || (lowY == 999)) {
 			lowY = y;
 		}
-		if((y > highY) || (highY == -999)) {
-			highY = y;			
+		if ((y > highY) || (highY == -999)) {
+			highY = y;
 		}
 	}
-
 }
