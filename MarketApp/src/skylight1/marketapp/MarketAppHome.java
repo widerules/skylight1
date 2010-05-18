6aa7dda1d447f10e7f46aeb87d31458285a70c43
megbeckIM <megbeckIM@9ff7648e-257f-11de-9b37-5dcb32d212a7>
2010-05-18 00:15:12 +0000

    initial commit

---
+++ MarketApp/src/skylight1/marketapp/MarketAppHome.java
@@ -0,0 +1,13 @@
+package skylight1.marketapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MarketAppHome extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
