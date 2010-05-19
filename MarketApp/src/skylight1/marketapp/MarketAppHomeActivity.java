84463b44810ffcc81c906b2c1411e0dc849d0937
megbeckIM <megbeckIM@9ff7648e-257f-11de-9b37-5dcb32d212a7>
2010-05-19 02:32:36 +0000

    added roboguice to the home screen too

--- MarketApp/src/skylight1/marketapp/MarketAppHomeActivity.java
+++ MarketApp/src/skylight1/marketapp/MarketAppHomeActivity.java
@@ -1,19 +1,24 @@
 package skylight1.marketapp;
 
-import android.app.Activity;
+import roboguice.activity.GuiceActivity;
+import roboguice.inject.InjectView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MarketAppHomeActivity extends Activity {
+public class MarketAppHomeActivity extends GuiceActivity {
+	@InjectView(R.id.watchListButton)
+	private Button watchListButton;
+
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.home);
 
-		findViewById(R.id.watchListButton).setOnClickListener(new OnClickListener() {
+		watchListButton.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View aV) {
 				Intent intent = new Intent(MarketAppHomeActivity.this, WatchListActivity.class);
