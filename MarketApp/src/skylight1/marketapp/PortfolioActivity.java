8a744ec8021305f5d0e04f22241d9cf2692e5cf0
mmellinger66 <mmellinger66@9ff7648e-257f-11de-9b37-5dcb32d212a7>
2010-06-03 23:30:35 +0000

    Adding updated PortfolioActivity

--- MarketApp/src/skylight1/marketapp/PortfolioActivity.java
+++ MarketApp/src/skylight1/marketapp/PortfolioActivity.java
@@ -4,16 +4,22 @@ import android.app.ListActivity;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
+
+
 
 /**
  * Created by IntelliJ IDEA.
@@ -22,24 +28,30 @@ import java.util.List;
  * Time: 7:57:45 PM
  */
 public class PortfolioActivity extends ListActivity {
+	
+	  @Override  
+	   public void onCreateContextMenu(ContextMenu menu, View v,ContextMenuInfo menuInfo) {  
+	super.onCreateContextMenu(menu, v, menuInfo);  
+	    menu.setHeaderTitle("Context Menu");  
+	    menu.add(0, v.getId(), 0, "Action 1");  
+	    menu.add(0, v.getId(), 0, "Action 2");  
+	}  
 
 //    private static final String[] DATA = {"AAPL", "GOOG"};
     static List<PortfolioItem> portfolioItems = new ArrayList<PortfolioItem>();
 
     private static class EfficientAdapter extends BaseAdapter {
         private LayoutInflater mInflater;
-        private Bitmap mIcon1;
-        private Bitmap mIcon2;
+       
 
         public EfficientAdapter(Context context) {
             // Cache the LayoutInflate to avoid asking for a new one each time.
             mInflater = LayoutInflater.from(context);
 
             // Icons bound to the rows.
-            mIcon1 = BitmapFactory.decodeResource(context.getResources(), R.drawable.icon);
-            mIcon2 = BitmapFactory.decodeResource(context.getResources(), R.drawable.icon);
-        }
-
+                 }
+      
+        
         /**
          * The number of items in the list is determined by the number of speeches
          * in our array.
@@ -81,7 +93,7 @@ public class PortfolioActivity extends ListActivity {
             // A ViewHolder keeps references to children views to avoid unneccessary calls
             // to findViewById() on each row.
             ViewHolder holder;
-
+            
             // When convertView is not null, we can reuse it directly, there is no need
             // to reinflate it. We only inflate a new View when the convertView supplied
             // by ListView is null.
@@ -95,23 +107,24 @@ public class PortfolioActivity extends ListActivity {
                 holder.avgPriceTextView = (TextView) convertView.findViewById(R.id.avgPrice);
                 holder.numberOfSharesTextView = (TextView) convertView.findViewById(R.id.numberOfShares);
                 holder.currentPriceTextView = (TextView) convertView.findViewById(R.id.currentPrice);
-//                holder.icon = (ImageView) convertView.findViewById(R.id.icon);
+               
 
                 convertView.setTag(holder);
             } else {
                 // Get the ViewHolder back to get fast access to the TextView
                 // and the ImageView.
                 holder = (ViewHolder) convertView.getTag();
+                
             }
 
             // Bind the data efficiently with the holder.
+            holder.avgPriceTextView.setTextColor(Color.GREEN);
             final PortfolioItem item = portfolioItems.get(position);
             holder.tickerTextView.setText( item.getTicker());
             holder.avgPriceTextView.setText( item.getAveragePriceStr());
             holder.numberOfSharesTextView.setText( item.getNumberOfSharesAsStr());
             holder.currentPriceTextView.setText( item.getCurrentPriceStr());
             
-            holder.icon.setImageBitmap((position & 1) == 1 ? mIcon1 : mIcon2);
 
             return convertView;
         }
@@ -121,12 +134,13 @@ public class PortfolioActivity extends ListActivity {
             TextView avgPriceTextView;
             TextView numberOfSharesTextView;
             TextView currentPriceTextView;
-            ImageView icon;
-        }
+           }
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+    
+         
         super.onCreate(savedInstanceState);
 //        setContentView(R.layout.portfolio);
         portfolioItems.add(new PortfolioItem("AAPL", 202.0f, 1000, 250.10f));
