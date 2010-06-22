03b390931a946f931c3b8dbca329908102db8e33
reccardt <reccardt@9ff7648e-257f-11de-9b37-5dcb32d212a7>
2010-06-22 18:02:56 +0000

    JDO experiments

--- MarketAppWeb/src/skylight1/marketappweb/MarketAppWebServlet.java
+++ MarketAppWeb/src/skylight1/marketappweb/MarketAppWebServlet.java
@@ -19,55 +19,75 @@ import skylight1.marketappweb.model.*;
 @SuppressWarnings({"unchecked","serial"})
 public class MarketAppWebServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
-			throws IOException {
+	throws IOException {
 		resp.setContentType("text/plain");
 		resp.getWriter().println("Hello, world");
-		// Let's make and persist a few items
-        PersistenceManager pm = PMF.get().getPersistenceManager();
-        
-        try {
-            // First clear out any from previous session:
-            String query = "select from " + WatchListItem.class.getName();
-            List<WatchListItem> items = (List<WatchListItem>) pm.newQuery(query).execute();
-            pm.deletePersistentAll(items);
+		PersistenceManager pm = PMF.get().getPersistenceManager();
 
-            query = "select from " + WatchList.class.getName();
-            List<WatchList> lists = (List<WatchList>) pm.newQuery(query).execute();
-            pm.deletePersistentAll(lists);
-            
-            WatchList wl = new WatchList("Tech Stocks","aardvark");
-            wl.getItems().add(new WatchListItem("goog"));
-            wl.getItems().add(new WatchListItem("msft"));
-            wl.getItems().add(new WatchListItem("IBM"));
-            pm.makePersistent(wl);
+		try {
+			// First clear out any from previous session:
+			String query = "select from " + WatchListItem.class.getName();
+			List<WatchListItem> items = (List<WatchListItem>) pm.newQuery(query).execute();
+			pm.deletePersistentAll(items);
 
-            wl = new WatchList("Energy Stocks","aardvark");
-            wl.getItems().add(new WatchListItem("BP"));
-            wl.getItems().add(new WatchListItem("XOM"));
-            wl.getItems().add(new WatchListItem("goog"));
-            pm.makePersistent(wl);
+			query = "select from " + WatchList.class.getName();
+			List<WatchList> lists = (List<WatchList>) pm.newQuery(query).execute();
+			pm.deletePersistentAll(lists);
 
-        } finally {
-            pm.close();
-        }
-        // Now pretend we're in another part of the app and we want to retrieve them:
-        pm = PMF.get().getPersistenceManager();
-        try {
-            String query = "select from " + WatchList.class.getName() + " where user == 'aardvark'" ;
-            List<WatchList> lists = (List<WatchList>) pm.newQuery(query).execute();
-            for(WatchList l : lists) {
-            	resp.getWriter().println("\nItems in " + l.getListName());
-            	for(WatchListItem wli : l.getItems())
-            		resp.getWriter().println("\t" + wli.toString());
-            }
-            
-            resp.getWriter().println("\nAll items:");
-            query = "select from " + WatchListItem.class.getName();
-            List<WatchListItem> items = (List<WatchListItem>) pm.newQuery(query).execute();
-            for(WatchListItem wli : items)
-            	resp.getWriter().println("\t" + wli.toString());
-       } finally {
-            pm.close();
-        }
- 	}
+			// Let's make and persist a few items
+			WatchList wl = new WatchList("Tech Stocks","aardvark");
+			wl.getItems().add(new WatchListItem("goog"));
+			wl.getItems().add(new WatchListItem("msft"));
+			wl.getItems().add(new WatchListItem("IBM"));
+			pm.makePersistent(wl);
+
+			wl = new WatchList("Energy Stocks","aardvark");
+			wl.getItems().add(new WatchListItem("BP"));
+			wl.getItems().add(new WatchListItem("XOM"));
+			wl.getItems().add(new WatchListItem("goog"));
+			pm.makePersistent(wl);
+
+		} finally {
+			pm.close();
+		}
+		showAll(resp);
+		resp.getWriter().println("\nNow try to remove Exxon.");
+		pm = PMF.get().getPersistenceManager();
+		try {
+			String query = "select from " + WatchList.class.getName() + " where user == 'aardvark' && listName == 'Energy Stocks'" ;
+			List<WatchList> lists = (List<WatchList>) pm.newQuery(query).execute();
+			for(WatchList l : lists) {
+				for(WatchListItem wli : l.getItems())
+					if(wli.getTicker().equals("XOM")) {
+						l.getItems().remove(wli);
+						break;
+					}
+			}
+		} finally {
+			pm.close();
+		}
+		showAll(resp);
+
+	}
+	private void showAll(HttpServletResponse resp) throws IOException {
+		// Now pretend we're in another part of the app and we want to retrieve them:
+		PersistenceManager pm = PMF.get().getPersistenceManager();
+		try {
+			String query = "select from " + WatchList.class.getName() + " where user == 'aardvark'" ;
+			List<WatchList> lists = (List<WatchList>) pm.newQuery(query).execute();
+			for(WatchList l : lists) {
+				resp.getWriter().println("\nItems in " + l.getListName());
+				for(WatchListItem wli : l.getItems())
+					resp.getWriter().println("\t" + wli.toString());
+			}
+
+			resp.getWriter().println("\nAll items:");
+			query = "select from " + WatchListItem.class.getName();
+			List<WatchListItem> items = (List<WatchListItem>) pm.newQuery(query).execute();
+			for(WatchListItem wli : items)
+				resp.getWriter().println("\t" + wli.toString());
+		} finally {
+			pm.close();
+		}
+	}
 }
