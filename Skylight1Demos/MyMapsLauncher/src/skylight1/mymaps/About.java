9a65d26192e3da585eec5b155f47694baf78f8cf
morrison.chang <morrison.chang@9ff7648e-257f-11de-9b37-5dcb32d212a7>
2010-09-30 23:52:10 +0000

    Initial check in.

---
+++ Skylight1Demos/MyMapsLauncher/src/skylight1/mymaps/About.java
@@ -0,0 +1,13 @@
+package skylight1.mymaps;
+
+import skylight1.mymaps.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class About extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.about);
+	}
+}
