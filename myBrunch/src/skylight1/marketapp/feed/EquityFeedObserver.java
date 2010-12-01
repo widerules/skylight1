6bb69ba869d904a8581b2e7d79650143b8aa590b
dario.nycjava <dario.nycjava@gmail.com>
2010-12-01 07:33:54 +0000

    fixed bug on saving local high scores in SuccessActivity (check first!)

--- myBrunch/src/skylight1/marketapp/feed/EquityFeedObserver.java
+++
@@ -1,9 +0,0 @@
-package skylight1.marketapp.feed;
-
-import java.util.Set;
-
-import skylight1.marketapp.model.EquityPricingInformation;
-
-public interface EquityFeedObserver {
-	void equityPricingInformationUpdate(Set<EquityPricingInformation> aSetOfEquityPricingInformation);
-}
