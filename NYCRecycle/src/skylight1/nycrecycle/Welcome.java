a33dea6790e1ca9fd7d9f0cc5e9bff5d38dfd66f
morrison.chang <morrison.chang@9ff7648e-257f-11de-9b37-5dcb32d212a7>
2011-01-24 03:16:23 +0000

    Initial empty version.

---
+++ NYCRecycle/src/skylight1/nycrecycle/Welcome.java
@@ -0,0 +1,13 @@
+package skylight1.nycrecycle;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Welcome extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
