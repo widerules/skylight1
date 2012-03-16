fc29b0b9d0f081daa866067a15b2586556050519
megbeckIM <megbeckIM@9ff7648e-257f-11de-9b37-5dcb32d212a7>
2012-03-16 23:49:48 +0000

    Initial import.

---
+++ NewEatsNewYorkPOC/src/org/skylight1/neny/model/Grade.java
@@ -0,0 +1,26 @@
+package org.skylight1.neny.model;
+
+public enum Grade {
+	GRADE_A("A"), GRADE_B("B"), GRADE_C("C"), GRADE_PENDING("Z"), NOT_YET_GRADED("");
+
+	private String code;
+
+	private Grade(final String aCode) {
+		code = aCode;
+	}
+
+	/**
+	 * Returns the grade associated with the code, or else null if the code is not that of a valid grade, e.g., "X".
+	 * 
+	 * @param aCode
+	 * @return
+	 */
+	public static Grade findByCode(final String aCode) {
+		for (final Grade grade : values()) {
+			if (grade.code.equals(aCode)) {
+				return grade;
+			}
+		}
+		return null;
+	}
+}
