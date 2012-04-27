a0c384403f760256a73605767c033b34e0d5a0ed
megbeckIM <megbeckIM@9ff7648e-257f-11de-9b37-5dcb32d212a7>
2012-04-27 01:12:45 +0000

    new logo from tommy; added times button; gets and shows restaurants from internet!!!!

--- NewEatsNewYork/src/org/skyight1/neny/android/NewEatsNewYorkActivity.java
+++ NewEatsNewYork/src/org/skyight1/neny/android/NewEatsNewYorkActivity.java
@@ -1,7 +1,9 @@
 package org.skyight1.neny.android;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class NewEatsNewYorkActivity extends Activity {
 	/** Called when the activity is first created. */
@@ -9,5 +11,11 @@ public class NewEatsNewYorkActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.home_view);
+
+	}
+
+	public void showRestaurants(final View aButton) {
+		final Intent showRestaurantsIntent = new Intent(this, ShowRestaurantListActivity.class);
+		startActivity(showRestaurantsIntent);
 	}
 }
