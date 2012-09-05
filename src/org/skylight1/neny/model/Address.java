5f8257075e9c3a459896750a071f1e39414942f2
msemsel <msemsel@gmail.com>
2012-09-05 19:27:20 +0000

    MS Server side code updates:
    Address.java	added @Embeddable annotation
    Borough.java	added static findByName(...) (not used on the server side, but keeping in line with Android client side version)
    Grade.java		added static findByName(...) (keeping in line with Android version)
    EMF.java 		added 5 more restaurants, now 7 restaurants cover all boroughs and a few cuisines.  Will do a lot more with source data.
    InspectionResultsFetcher.java	mostly added comments, added some static final int values to help me match those parsed groups

--- src/org/skylight1/neny/model/Address.java
+++ src/org/skylight1/neny/model/Address.java
@@ -1,7 +1,10 @@
 package org.skylight1.neny.model;
 
 import static java.lang.String.format;
+import javax.persistence.Embeddable;
 
+// MS - added Embeddable tag so that Address object in Restaurant object can be persisted
+@Embeddable
 public class Address {
 	private String building;
 
