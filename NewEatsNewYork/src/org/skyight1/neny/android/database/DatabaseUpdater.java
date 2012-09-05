e8932d2819701e2952cf5837a2272595779adaf8
msemsel <msemsel@gmail.com>
2012-09-05 17:28:10 +0000

    Mark Semsel branch

--- NewEatsNewYork/src/org/skyight1/neny/android/database/DatabaseUpdater.java
+++
@@ -1,5 +0,0 @@
-package org.skyight1.neny.android.database;
-
-public class DatabaseUpdater {
-
-}
--- SkylightGame/src/net/nycjava/skylight1/service/BalancedObjectPublicationService.java
+++
@@ -1,8 +0,0 @@
-package net.nycjava.skylight1.service;
-
-public interface BalancedObjectPublicationService extends Observable<BalancedObjectObserver> {
-	void applyForce(float anXForce, float aYForce, long aDuration);
-	void setDifficultyLevel(int aDifficulty);
-	void startService();
-	void stopService();
-}
