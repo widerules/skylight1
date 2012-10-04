0ac67f8bd9560cb49789720a3a935448ca205e48
sajit.kunnumkal <sajit.kunnumkal@gmail.com>
2012-10-04 16:01:15 +0000

    Test commit

--- NewEatsNewYork/src/org/skyight1/neny/android/SelectDiningTimesActivity.java
+++ NewEatsNewYork/src/org/skyight1/neny/android/SelectDiningTimesActivity.java
@@ -2,12 +2,15 @@ package org.skyight1.neny.android;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class SelectDiningTimesActivity extends Activity{
 
+	private static final String TAG ="SelectDiningTimesActivity";
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.time_view);
+		Log.i(TAG, "Select Dining Activities");
 	}
 }
