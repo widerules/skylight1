80336164bfd3ced7efeac21d479113172fc4149d
schandler25 <schandler25@gmail.com>
2013-05-01 02:08:50 +0000

    fixed images in dining times screen; deleted dead code

--- NewEatsNewYork/tools/fix.pl
+++ NewEatsNewYork/tools/fix.pl
@@ -3,10 +3,12 @@ while(<>) {
     $old = $_;
     $a = lc ($_);
     $a =~ s/-/_/g;
-    if($a =~ /inactive/) {
-	$a =~ s/inactive/active/g;
-    } else {
-        $a =~ s/active/inactive/g;
-    }
+    if ($a !~ /breakfast/ && $a !~ /dinner/) {
+	    if($a =~ /inactive/) {
+			$a =~ s/inactive/active/g;
+  	  	} else {
+   	    	$a =~ s/active/inactive/g;
+   	 	}
+   	}
     print "cp hires-art-assets/$old res/drawable-hdpi/$a\n";
 }
